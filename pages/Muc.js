4738e89b536e948d8cbb77a88899438a377dd210
Minh <tranminh200199@gmail.com>
2022-03-16 14:48:55 +0700

    commit cua Minh

--- pages/Muc.js
+++ pages/Muc.js
@@ -3,6 +3,9 @@ import Layout from "../partial/Layout";
 
 export default function Tin(){
     return(
+        <div>
         <Mucbody />
+        <span>dan lieu oc heooooooooooooo</span>
+        </div>
     )
 }
