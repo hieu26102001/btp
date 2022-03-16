889eb7979773d7009952823d662bd65bbeb025d9
Nguyen Duc Hieu <86180191+hieu26102001@users.noreply.github.com>
2022-03-16 14:47:31 +0700

    Xoa ABC

--- component/Card/RowCard.jsx
+++ component/Card/RowCard.jsx
@@ -12,14 +12,7 @@ export default function Rowcard({ item, w, h,ActivePara, ActiveImg,Active }) {
                         <div className={ActiveImg?"md:flex hidden":"hidden"}>
                         <Image src={item.images} width={w} height={h} />
                         </div>
-                        <div className={ActiveImg?"w-3/5":""}>
-                            <div className={`${Active?"hidden":""} `}>
-                                {item.article}
-                            </div>
-                            <div className="">
-                                <div className={ActivePara?"font-normal text-justify md:flex hidden":"hidden"}>{item.para}</div>
-                            </div>
-                        </div>
+
                     </div>
                 </a>
             </Link>
